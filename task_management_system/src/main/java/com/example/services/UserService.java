00dbfd26aa36e47705bed384dd8c095f78c0db2e
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-12-21 19:33:48 +0530

    Add UserService

--- task_management_system/src/main/java/com/example/services/UserService.java
+++ task_management_system/src/main/java/com/example/services/UserService.java
@@ -1,4 +1,12 @@
 package com.example.services;
 
+import com.example.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User createUser(User user);
+    User getUserById(Long id);
+    List<User> getAllUsers();
+    void deleteUser(Long id);
 }
--- task_management_system/src/main/java/com/example/services/impl/UserServiceImpl.java
+++ task_management_system/src/main/java/com/example/services/impl/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package com.example.services.impl;
 
-public class UserServiceImpl {
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import com.example.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+    public UserServiceImpl(UserRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public User createUser(User user){
+        return repository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id){
+        return repository.findById(id).orElseThrow(
+                () -> new RuntimeException("User not found")
+        );
+    }
+
+    @Override
+    public List<User> getAllUsers(){
+        return repository.findAll();
+    }
+
+    @Override
+    public void deleteUser(Long id){
+        repository.delete(getUserById(id));
+    }
 }
