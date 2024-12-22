c8be1e2402799e5e42c4615ba1a600680c1009ea
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-12-22 22:33:55 +0530

    Add task repo tests

--- task_management_system/src/test/java/com/example/repository/TaskRepositoryTest.java
+++ task_management_system/src/test/java/com/example/repository/TaskRepositoryTest.java
@@ -1,13 +1,56 @@
 package com.example.repository;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import com.example.model.Task;
+import com.example.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
 class TaskRepositoryTest {
+    private User user;
+    private Task task;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+        user.setUsername("user");
+        user.setPassword("password");
+        user.setEmailId("testUser@test.com");
+
+        task = new Task();
+        task.setTitle("test title");
+        task.setUser(user);
+        task.setDueDate(LocalDate.of(2100, 12, 12));
+
+        userRepository.save(user);
+        taskRepository.save(task);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        taskRepository.delete(task);
+        userRepository.delete(user);
+    }
+
+    @Test
+    void givenTaskWhenSavedThenCanBeFoundById() {
+        Task savedTask = taskRepository.findById(task.getId()).orElse(null);
+        assertNotNull(savedTask);
+        assertEquals(task.getTitle(), savedTask.getTitle());
+        assertEquals(task.getUser(), savedTask.getUser());
+        assertEquals(task.getDueDate(), savedTask.getDueDate());
+    }
 }
